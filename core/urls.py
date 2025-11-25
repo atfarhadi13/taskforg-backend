44f12dd7f2b38dc822b4e975dd6095fd301ac8b3
atfarhadi13 <atfarhadi313@gmail.com>
2025-11-25 11:01:49 +0430

    healthcheck endpoint and basic docker compose yml for postgres

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import health_check
+
+urlpatterns = [
+    path("health/", health_check, name="health-check"),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.db import connections
+from django.db.utils import OperationalError
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(["GET"])
+def health_check(request):
+    db_ok = True
+    try:
+        db_conn = connections["default"]
+        db_conn.cursor()
+    except OperationalError:
+        db_ok = False
+
+    data = {
+        "status": "ok",
+        "database": "ok" if db_ok else "error",
+    }
+
+    return Response(
+        data,
+        status=status.HTTP_200_OK if db_ok else status.HTTP_503_SERVICE_UNAVAILABLE,
+    )
