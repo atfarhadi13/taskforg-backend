16bdc15c8eccad972e05b3fcaf95f6eb46082215
atfarhadi13 <atfarhadi313@gmail.com>
2025-11-25 10:51:31 +0430

    done with day 4 create reusable base models in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("-created_at",)
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModel(UUIDModel, TimeStampedModel):
+    class Meta:
+        abstract = True
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+class Project(BaseModel):
+    name = models.CharField(max_length=255)
